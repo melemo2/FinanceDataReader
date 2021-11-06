96dd4ef56cc00bb65cd91c08a4638dc4e4e2aafc
melemo2 <sonmingi0123@gmail.com>
2021-11-06 17:42:49 +0900

    no message

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='FinanceDataReader',
+    url='https://github.com/melemo2/FinanceDataReader.git',
     version='0.1',
     packages=find_packages(),
 )
