e780ade155ea12fbdbab7d2f4b005f9e68ab6457
melemo2 <sonmingi0123@gmail.com>
2021-11-06 17:35:32 +0900

    no message

---
+++ setup.py
@@ -0,0 +1,9 @@
+from glob import glob
+from os.path import basename, splitext
+from setuptools import find_packages, setup
+
+setup(
+    name='FinanceDataReader',
+    version='0.1',
+    packages=find_packages(),
+)
